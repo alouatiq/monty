592c944de49cce13b5d7b8a7db224ea5fb0d8e57
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-25 14:36:01 +0200

    Update push.c

--- src/push.c
+++ src/push.c
@@ -1,7 +1,7 @@
-#include "monty.h"
-
 void push(stack_t **stack, unsigned int line_number, int n)
 {
+    (void)line_number;  // Mark the parameter as unused
+
     stack_t *new_node = malloc(sizeof(stack_t));
     if (!new_node)
     {
