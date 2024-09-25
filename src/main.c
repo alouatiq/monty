cec744c42ab289208c19d62783f90bcbb93d6c22
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-25 14:37:07 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 int main(int argc, char **argv)
 {
-    FILE *file;  // Declaration moved to the top
+    FILE *file;  /* Declaration moved to the top */
 
     if (argc != 2)
     {
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    file = fopen(argv[1], "r");  // Now only the assignment happens here
+    file = fopen(argv[1], "r");  /* Now only the assignment happens here */
     if (!file)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
