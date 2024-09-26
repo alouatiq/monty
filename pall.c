77849220189de7f021d85d8960e22bd296d9cd03
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-26 10:47:54 +0200

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ * pall - Prints all the values on the stack, starting from the top.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: The current line number in the bytecode file.
+ */
+
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current;  /* Move the declaration to the top */
