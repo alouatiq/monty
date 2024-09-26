4fbf0dc76b3979a43c0d4c5ae90d212668eb451f
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-26 10:47:31 +0200

    Update nop.c

--- nop.c
+++ nop.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ * nop - Does nothing.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: The current line number in the bytecode file.
+ */
+
 void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
