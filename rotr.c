b06bc0d3a8ae70569efb47ab3815292f4db4bbc2
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-26 14:13:40 +0200

    Create rotr.c

---
+++ rotr.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * rotr - Rotates the stack to the bottom.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: The current line number (unused).
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+    stack_t *tail;
+
+    (void)line_number;
+
+    if (*stack == NULL || (*stack)->next == NULL)
+        return;
+
+    tail = *stack;
+
+    while (tail->next)  /* Traverse to the last node */
+        tail = tail->next;
+
+    tail->prev->next = NULL;  /* Unlink the last node from the list */
+    tail->prev = NULL;
+    tail->next = *stack;  /* Move the last node to the top */
+    (*stack)->prev = tail;
+    *stack = tail;
+}
