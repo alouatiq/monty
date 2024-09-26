85494d04d1e1f3e9dd91ff169f5f18bda7c37f79
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-26 11:00:46 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include "monty.h"   /* For custom functions and stack_t */
 
 void process_line(char *line, unsigned int line_number, stack_t **stack);
+int is_number(char *str);  /* Declare is_number function */
 
 /**
  * main - Entry point of the Monty bytecode interpreter.
@@ -11,13 +12,12 @@ void process_line(char *line, unsigned int line_number, stack_t **stack);
  * @argv: Argument vector.
  * Return: 0 on success, EXIT_FAILURE on failure.
  */
-
 int main(int argc, char **argv)
 {
     FILE *file;
     char *line = NULL;
     size_t len = 0;
-    ssize_t read;
+    int read;
     unsigned int line_number = 0;
     stack_t *stack = NULL;  /* Initialize stack to NULL */
 
@@ -34,8 +34,8 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    /* Read the file line by line */
-    while ((read = getline(&line, &len, file)) != -1)
+    /* Read the file line by line using fgets as getline is not available in C89 */
+    while (fgets(line, len, file) != NULL)
     {
         line_number++;
         process_line(line, line_number, &stack);
@@ -92,10 +92,12 @@ void process_line(char *line, unsigned int line_number, stack_t **stack)
  */
 int is_number(char *str)
 {
+    int i;
+
     if (str == NULL || *str == '\0')
         return (0);
     
-    for (int i = 0; str[i]; i++)
+    for (i = 0; str[i]; i++)  /* Declare i at the top of the block */
     {
         if (str[i] < '0' || str[i] > '9')
             return (0);
