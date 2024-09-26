9d65149b1656f7eb4d244d3551ec8d8f11d485d9
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-26 10:40:05 +0200

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,10 +1,12 @@
-#include "monty.h" 
+#include "monty.h"
 
 void pall(stack_t **stack, unsigned int line_number)
 {
+    stack_t *current;  /* Move the declaration to the top */
+
     (void)line_number;  /* Mark line_number as unused for now */
 
-    stack_t *current = *stack;
+    current = *stack;   /* Now assign current after the declaration */
 
     while (current)
     {
