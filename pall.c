1871c86e76568f578e4026b95b3dca86b50d8628
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-26 11:10:44 +0200

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,22 +1,19 @@
 #include "monty.h"
 
 /**
-	* pall - Prints all the values on the stack, starting from the top.
-	* @stack: Double pointer to the top of the stack.
-	* @line_number: The current line number in the bytecode file.
-	*/
-
+ * pall - Prints all the values on the stack, starting from the top.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: The current line number in the bytecode file.
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current;  /* Move the declaration to the top */
+    stack_t *current = *stack;
 
-	(void)line_number;  /* Mark line_number as unused for now */
+    printf("Executing pall (line %d)\n", line_number);  /* Debugging output */
 
-	current = *stack;   /* Now assign current after the declaration */
-
-	while (current)
-	{
-	printf("%d\n", current->n);
-	current = current->next;
-	}
+    while (current)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
 }
