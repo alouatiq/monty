3f74ae3f6046ae49bc39b8d5f4804d26d46a6490
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-26 10:48:10 +0200

    Update pint.c

--- pint.c
+++ pint.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ * pint - Prints the value at the top of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: The current line number in the bytecode file.
+ */
+
 void pint(stack_t **stack, unsigned int line_number)
 {
 	if (!*stack)
