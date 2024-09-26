078591e76e85646d83a7411b48bb09e0cf58d462
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-26 11:37:56 +0200

    Update pall.c

--- pall.c
+++ pall.c
@@ -3,13 +3,13 @@
 /**
  * pall - Prints all the values on the stack, starting from the top.
  * @stack: Double pointer to the top of the stack.
- * @line_number: The current line number in the bytecode file.
+ * @line_number: The current line number in the bytecode file (unused).
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
     stack_t *current = *stack;
 
-    /* printf("Executing pall (line %d)\n", line_number); */  /* Comment out */
+    (void)line_number;  /* Mark line_number as unused */
 
     while (current)
     {
@@ -17,4 +17,3 @@ void pall(stack_t **stack, unsigned int line_number)
         current = current->next;
     }
 }
-
