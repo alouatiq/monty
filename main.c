0cf1efebc2810b99954c56c24a1e137f18f8c72b
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-26 11:16:53 +0200

    Update main.c

--- main.c
+++ main.c
@@ -33,8 +33,7 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    /* Read the file line by line using fgets */
-    /* as getline is not available in C89 */
+    /* Read file line by line using fgets, as getline is not available in C89 */
     while (fgets(line, len, file) != NULL)
     {
         line_number++;
@@ -45,4 +44,64 @@ int main(int argc, char **argv)
     free(line);
     fclose(file);
     return (0);
+}
+
+/**
+ * process_line - Parses a line and executes the corresponding opcode.
+ * @line: The line from the bytecode file.
+ * @line_number: The current line number.
+ * @stack: Pointer to the top of the stack.
+ */
+void process_line(char *line, unsigned int line_number, stack_t **stack)
+{
+    char *opcode;
+    int n;
+
+    opcode = strtok(line, " \n\t");
+    if (!opcode || opcode[0] == '#')  /* Ignore empty lines and comments */
+        return;
+
+    if (strcmp(opcode, "push") == 0)
+    {
+        char *arg = strtok(NULL, " \n\t");
+
+        if (!arg || !is_number(arg))
+        {
+            fprintf(stderr, "L%d: usage: push integer\n", line_number);
+            exit(EXIT_FAILURE);
+        }
+
+        n = atoi(arg);  /* Convert argument to an integer */
+        push(stack, line_number, n);
+    }
+    else if (strcmp(opcode, "pall") == 0)
+    {
+        pall(stack, line_number);
+    }
+    else
+    {
+        fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * is_number - Checks if a string is a number.
+ * @str: The string to check.
+ * Return: 1 if the string is a valid number, 0 otherwise.
+ */
+int is_number(char *str)
+{
+    int i;
+
+    if (str == NULL || *str == '\0')
+        return (0);
+    
+    for (i = 0; str[i]; i++)  /* Declare i at the top of the block */
+    {
+        if (str[i] < '0' || str[i] > '9')
+            return (0);
+    }
+
+    return (1);
 }
