000b28fb91d11fcab2875ef3cfbe3daaaf34c1f8
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-26 10:46:32 +0200

    Update add.c

--- add.c
+++ add.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ * add - Adds the top two elements of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: The current line number in the bytecode file.
+ */
+
 void add(stack_t **stack, unsigned int line_number)
 {
 	if (!*stack || !(*stack)->next)
