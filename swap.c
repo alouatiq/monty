1db1a4b413ec85a4c6edcdb1c77fcc9c8ddbbd4b
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-26 10:49:01 +0200

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ * swap - Swaps the top two elements of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: The current line number in the bytecode file.
+ */
+
 void swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *first, *second;
