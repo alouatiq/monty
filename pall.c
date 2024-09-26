403a67ca3358d1eae91d0d83a11bfbc6dcf953bd
AL OUATIQ <alouatiq@outlook.com>
2024-09-26 04:35:19 -0700

    testing

--- pall.c
+++ pall.c
@@ -1,19 +1,19 @@
 #include "monty.h"
 
 /**
- * pall - Prints all the values on the stack, starting from the top.
- * @stack: Double pointer to the top of the stack.
- * @line_number: The current line number in the bytecode file (unused).
- */
+	* pall - Prints all the values on the stack, starting from the top.
+	* @stack: Double pointer to the top of the stack.
+	* @line_number: The current line number in the bytecode file (unused).
+	*/
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
+	stack_t *current = *stack;
 
-    (void)line_number;  /* Mark line_number as unused */
+	(void)line_number;  /* Mark line_number as unused */
 
-    while (current)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	while (current)
+	{
+	printf("%d\n", current->n);
+	current = current->next;
+	}
 }
--- pop.c
+++ pop.c
@@ -1,25 +1,25 @@
 #include "monty.h"
 
 /**
- * pop - Removes the top element of the stack.
- * @stack: Double pointer to the top of the stack.
- * @line_number: The current line number.
- */
+	* pop - Removes the top element of the stack.
+	* @stack: Double pointer to the top of the stack.
+	* @line_number: The current line number.
+	*/
 void pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL)
+	{
+	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+	exit(EXIT_FAILURE);
+	}
 
-    temp = *stack;
-    *stack = (*stack)->next;
+	temp = *stack;
+	*stack = (*stack)->next;
 
-    if (*stack != NULL)
-        (*stack)->prev = NULL;
+	if (*stack != NULL)
+	(*stack)->prev = NULL;
 
-    free(temp);
+	free(temp);
 }
